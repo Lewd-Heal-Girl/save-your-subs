dc822bdfeef6424695ca29ed5dd7b772b9b12290
Lewd-Heal-Girl <lewd_heal_girl@proton.me>
2023-05-06 14:52:19 +0200

    much progress

---
+++ save_your_subs/process_post.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+import json
+from queue import Queue
+
+from .reddit import Post
+
+DATA_PATH = Path("subs-stashed-away")
+
+
+class Processor:
+    def __init__(
+            self,
+            reddit_queue: Queue,
+            imgur_queue: Queue,
+            general_queue: Queue,
+            data_path: Path = DATA_PATH
+    ):
+        self.reddit_queue = reddit_queue
+        self.imgur_queue = imgur_queue
+        self.general_queue = general_queue,
+
+        self.data_path = data_path
+
+    def process(self, post: Post):
+        print(post)
+
+        sub_path = Path(self.data_path, post.subreddit)
+        sub_path.mkdir(parents=True, exist_ok=True)
+
+        post_path = Path(sub_path, "posts")
+        post_path.mkdir(parents=True, exist_ok=True)
+
+        with Path(post_path, f"{post.id}.json").open("w") as f:
+            json.dump(post.json, f)
